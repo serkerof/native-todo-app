06328c33c0b5e2862f3ce6ab692e858e2bf53a24
syrmon <98706426+syrmon@users.noreply.github.com>
2022-05-11 11:11:38 +0200

    created header Template

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import styles from './headerStyle';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import {Icon} from 'react-native-elements';
 
 const Header = () => {
+  const dayBox = (
+    <View style={styles.dayBox}>
+      <Text style={styles.weekDay}>Sun</Text>
+      <Text style={styles.monthDay}>1</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.iconContainer}>
+        <Icon name="menu" type="material-icons" size={25} color="#fff" />
         <Icon name="add" type="material-icons" raised size={25} />
       </View>
 
-      <View></View>
-
-      <View></View>
+      <View style={styles.dateInfoContainer}>
+        <View>
+          <Text style={styles.dateYear}>2022</Text>
+          <Text style={styles.dateMonth}>May</Text>
+        </View>
+        <View style={styles.dayStreakContainer}>
+          <Text style={styles.dayStreakCount}>100</Text>
+          <Icon
+            name="local-fire-department"
+            type="material-icons"
+            size={30}
+            color="#E16B00"
+          />
+        </View>
+      </View>
+      <View style={styles.dayBoxContainer}>{dayBox}{dayBox}{dayBox}{dayBox}{dayBox}</View>
     </View>
   );
 };
--- src/components/header/headerStyle.js
+++ src/components/header/headerStyle.js
@@ -1,7 +1,68 @@
 import {StyleSheet} from 'react-native';
+import CSS_VARIABLES from '../../assets/styling/CssVariables';
 
 export default StyleSheet.create({
   container: {
+    flex: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 15,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     flex: 1,
+  },
+  dateInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+  },
+  dateYear: {
+    color: 'white',
+  },
+  dateMonth: {
+    color: 'white',
+    fontSize: 18,
+  },
+  dayStreakContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dayStreakCount: {
+    marginRight: 5,
+    fontSize: 18,
+    color: 'white',
+  },
+  dayBoxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 2,
+  },
+  dayBox: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#585DFF',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginRight: 5
+  },
+  weekDay: {
+    color: 'white',
+    fontSize: 16,
+    textTransform: 'uppercase',
+  },
+  monthDay: {
+    color: 'white',
+    fontSize: 16,
+  },
+  activeDay: {
+    backgroundColor: 'white',
+  },
+  fontBlue: {
+    color: CSS_VARIABLES.mainColor,
   },
 });
